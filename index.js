0f8fed294970ae26d3bc476e87dd8b0879f498c5
csmahmud99 <csmahmud.2021@gmail.com>
2023-06-06 01:40:27 +0600

    API for cart item number update --- Added

--- index.js
+++ index.js
@@ -39,10 +39,22 @@ async function run() {
             res.send(result);
         });
 
-        // cart collection
+        // cart collection APIs
+        app.get("/carts", async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            else {
+                const query = { email: email };
+                const result = await cartCollection.find(query).toArray();
+                res.send(result);
+            }
+        });
+
         app.post("/carts", async (req, res) => {
             const item = req.body;
-            console.log(item);
+            // console.log(item);
             const result = await cartCollection.insertOne(item);
             res.send(result);
         });
